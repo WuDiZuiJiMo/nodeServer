1d20ec60b50584d725b429049e3781ae80f42f90
WuDiZuiJiMo <30789111+WuDiZuiJiMo@users.noreply.github.com>
2022-05-20 15:02:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use(express.static('public'))
 
 // 处理请求头（跨域处理）
 app.all('*', function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", req.headers.origin || '*');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
     res.header("X-Powered-By", ' 3.2.1');
